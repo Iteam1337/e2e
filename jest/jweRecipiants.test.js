1ba0d2707c0ae9ef3f444ebc80783a47a49ab654
Mikael Gråborg <mikael.graborg@iteam.se>
2020-05-28 14:45:52 +0200

    test: add jwerecipients tests

---
+++ jest/jweRecipiants.test.js
@@ -0,0 +1,78 @@
+const { createClientWithServer } = require('./helpers/index')
+const phone = require('./helpers/phone')
+const postgres = require('./helpers/operatorPostgres')
+
+describe('jweRecipients', () => {
+  let serviceClient, connectionId, clientId
+
+  beforeAll(async () => {
+    await phone.clearAccount()
+
+    await phone.createAccount({ firstName: 'Foo', lastName: 'Barsson' })
+
+    const serviceConfig = {
+      defaultPermissions: [
+        {
+          area: 'favorite_cats',
+          types: ['WRITE'],
+          description: 'The cats you like the most'
+        },
+        {
+          area: 'favorite_cats',
+          types: ['READ'],
+          purpose: 'To recommend you cats that you\'ll like'
+        }
+      ]
+    }
+
+    serviceClient = await createClientWithServer(serviceConfig)
+    clientId = serviceClient.config.clientId
+    await serviceClient.connect()
+
+    const { url } = await serviceClient.initializeAuthentication()
+
+    // Send it to phone
+    const { connectionRequest } = await phone.handleAuthCode({ code: url })
+
+    // Approve it!
+    let approvalResponse = new Map()
+    connectionRequest.permissions.forEach(p => approvalResponse.set(p.id, true))
+    connectionId = await phone.approveConnection(connectionRequest, approvalResponse)
+    const data = ['All of them']
+    const area = 'favorite_cats'
+
+    await serviceClient.data.write(connectionId, { area, data })
+    const dataz = await serviceClient.data.read(connectionId, { area: 'favorite_cats' })
+    console.log(dataz, 'dataz')
+  })
+
+  afterAll(async (done) => {
+    await phone.clearAccount()
+    await postgres.clearOperatorDb()
+    await serviceClient.config.keyValueStore.removeAll()
+    serviceClient.server.close(done)
+  })
+
+  it('new service can read from old service', async () => {
+    const serviceConfig2 = {
+      defaultPermissions: [
+        {
+          area: 'favorite_cats',
+          types: ['READ'],
+          purpose: 'To recommend you cats that you\'ll like',
+          domain: clientId
+        }
+      ]
+    }
+    const serviceClient2 = await createClientWithServer(serviceConfig2)
+    await serviceClient2.connect()
+    const { url } = await serviceClient2.initializeAuthentication()
+    const { connectionRequest } = await phone.handleAuthCode({ code: url })
+    let approvalResponse = new Map()
+    connectionRequest.permissions.forEach(p => approvalResponse.set(p.id, true))
+    connectionId = await phone.approveConnection(connectionRequest, approvalResponse)
+
+    // Den anslutande servicen måste veta vilken domain som den gamla har registrerat sig med.
+    // Decode eller verify?
+  })
+})
